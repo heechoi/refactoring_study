2f32c0d5343febb811534baed5f7f893e5d61053
DaHee Choi <ekgml9904@naver.com>
2017-12-05 16:11:46 +0900

    리팩토리 메서드 추출

--- src/kr/or/dgit/refactoring_study/Customer.java
+++ src/kr/or/dgit/refactoring_study/Customer.java
@@ -26,36 +26,19 @@ public class Customer {
 		StringBuilder result = new StringBuilder(getName()+" 고객님의 대여기록\n");
 		
 		for(Rental each:rentals) {
-			double thisAmount=0;
-			switch(each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount +=2;
-				if(each.getDaysRented()>2)
-					thisAmount +=(each.getDaysRented()-2)*1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount+=each.getDaysRented()*3;
-				break;
-			case Movie.CHILRENS:
-				thisAmount+=1.5;
-				if(each.getDaysRented()>3)
-					thisAmount +=(each.getDaysRented()-3)*1.5;
-				break;
-			}
 			
-			frequentRenterPoints++;
+			frequentRenterPoints +=  each.getFrequentRenterPoint(); //계속 누적
 			
-			if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)&&each.getDaysRented()>1)
-				frequentRenterPoints++;
+			
 			result.append("\t"+each.getMovie().getTitle());
-			result.append("\t"+String.valueOf(thisAmount)+"\n");
+			result.append("\t"+String.valueOf( each.getCharge())+"\n");
 			
-			totalAmount +=thisAmount;
+			totalAmount += each.getCharge();
 		}
 		
 		result.append("누적 대여료 : "+String.valueOf(totalAmount)+"\n");
 		result.append("적립 포인트 : "+String.valueOf(frequentRenterPoints));
 		
 		return result.toString();
-	}
+	}	
 }
